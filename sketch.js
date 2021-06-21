33395c031ea626d12dddad2a141820efd7b53b9d
POKEMON211 <30438481+POKEMON211@users.noreply.github.com>
2021-06-21 16:57:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ship, move_ship;
+var sea, seaI;
+
+function preload(){
+  move_ship = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+  seaI = loadImage("sea.png");
+
+}
+
+
+function setup(){
+  createCanvas(800, 800);
+
+  sea = createSprite(50, 50, 100, 100); 
+  sea.addImage(seaI);
+  sea.scale = 0.3;
+
+  ship = createSprite(250, 120, 50, 50);
+  ship.addAnimation("move", move_ship);
+  ship.scale = 0.3;
+
+}
+
+function draw() {
+  background("white");
+  drawSprites();  
+ 
+}
